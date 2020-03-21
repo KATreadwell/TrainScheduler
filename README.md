f1a5c7e80ca60d6d136521ac1328d8b0bb5dcbd7
KAT <katreadwell1@gmail.com>
2020-03-21 16:00:31 -0700

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TrainScheduler
+
+Hello Train Conductor!  Are you having trouble adding trains to the train schedule?  With this nifty scheduler, easily add incoming trains and keep track of arrivals and departures!
+
+Remember: Anytime is Train Time!  Choo Choo.  Chee Chee.
---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD9a8FeQuBkK2FSJBEd847V13dZ4G4R0nQ",
+    authDomain: "trainscheduler-e81d0.firebaseapp.com",
+    databaseURL: "https://trainscheduler-e81d0.firebaseio.com",
+    projectId: "trainscheduler-e81d0",
+    storageBucket: "trainscheduler-e81d0.appspot.com",
+    messagingSenderId: "728984868764",
+    appId: "1:728984868764:web:1360d2e3ee7333de395a59"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
