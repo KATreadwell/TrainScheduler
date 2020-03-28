b6016321a99b003fe489f1b4b50089da0f9f8e7a
KAT <katreadwell1@gmail.com>
2020-03-28 08:00:44 -0700

    refresh now works, gave up on authentication, need to add del/edit actions

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Hello Train Conductor!  Are you having trouble adding trains to the train schedule?  With this nifty scheduler, easily add incoming trains and keep track of arrivals and departures!
 
 Remember: Anytime is Train Time!  Choo Choo.  Chee Chee.
+
+Features:
+1. Updates once per minute.
+2. Validation enforced so that user must fill out all fields prior to adding a train.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,27 +8,26 @@ var firebaseConfig = {
     appId: "1:728984868764:web:1360d2e3ee7333de395a59"
 };
 
-//Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+// //Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// var database = firebase.database();
 
-//use Google for authentication
-var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-  });
+// // //use Google for authentication
+// var provider = new firebase.auth().GoogleAuthProvider();
+// firebase.auth().signInWithPopup(provider).then(function(result) {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     var token = result.credential.accessToken;
+//     // The signed-in user info.
+//     var user = result.user;
+//   }).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // The email of the user's account used.
+//     var email = error.email;
+//     // The firebase.auth.AuthCredential type that was used.
+//     var credential = error.credential;
+//   });
 
 
 //on press of "Submit", store values in dB and push input values to page
@@ -79,10 +78,32 @@ database.ref().on("child_added", function (snapshot) {
         $("<td>").text(train.frequency),
         $("<td>").text(moment(nextArrival).format("HH:mm")),
         $("<td>").text(minAway),
-        $("<td>").append('<button class="btn"><i class="fa fa-trash"></i></button>'),
-        $("<td>").append('<button class="btn"><i class="fa fa-pencil"></i></button>'),
+        $("<td>").append(`<button class="btn trash" trainId=${snapshot.key}><i class="fa fa-trash"></i></button>`),
+        $("<td>").append(`<button class="btn pencil" trainId=${snapshot.key}><i class="fa fa-pencil"></i></button>`),
     );
-
+        console.log(snapshot.key);
     $("#currentTable").append(rowInfo);  
 })
 
+
+setInterval(function(){
+    $("#currentTable").empty();
+    database.ref().on("child_added", function (snapshot) {
+        var train = snapshot.val();
+        var diffTime = moment().diff(moment.unix(train.time), "minutes");
+        var remainder = diffTime % train.frequency;
+        var minAway = train.frequency - remainder;
+        var nextArrival = moment().add(minAway, "minutes");
+        var rowInfo = $("<tr>").append(
+            $("<td>").text(train.trainName),
+            $("<td>").text(train.destination),
+            $("<td>").text(train.frequency),
+            $("<td>").text(moment(nextArrival).format("HH:mm")),
+            $("<td>").text(minAway),
+            $("<td>").append('<button class="btn"><i class="fa fa-trash"></i></button>'),
+            $("<td>").append('<button class="btn"><i class="fa fa-pencil"></i></button>'),
+        );
+    
+        $("#currentTable").append(rowInfo);  
+    })
+}, 5000);
